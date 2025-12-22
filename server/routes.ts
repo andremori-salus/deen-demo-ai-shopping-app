1a42c63b228aaf8acd5ba9cdfa6bda3c035e35d6
ディーン <deenhans@gmail.com>
2025-12-22 16:34:03 +0200

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -4,9 +4,14 @@ import { storage } from "./storage";
 
 export async function registerRoutes(app: Express): Promise<Server> {
   app.get("/api/products", async (_req, res) => {
-    const products = await storage.getProducts();
-    const sorted = products.sort((a, b) => a.price - b.price);
-    res.json(sorted[0].relatedProducts.filter(id => id > 0));
+    try {
+      const products = await storage.getProducts();
+      const sorted = products.sort((a, b) => a.price - b.price);
+      res.json(sorted[0].relatedProducts.filter(id => id > 0));
+    } catch (error) {
+      console.error(JSON.stringify(error));
+      throw error;
+    }
   });
 
   app.get("/api/products/:id", async (req, res) => {
